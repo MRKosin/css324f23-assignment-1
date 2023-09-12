83416cf5bdd7e1ff0cbe1dc6c550eceecaa9e19f
MRKosin <144799514+MRKosin@users.noreply.github.com>
2023-09-12 18:45:47 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Kosin Kalarat", "sid": "6622300090"},
+    {"name": "", "sid": ""},
 ]
